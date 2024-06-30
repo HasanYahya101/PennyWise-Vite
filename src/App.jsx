91a4ada344d5c3844a793352014cd1d07617c6d3
Hasan Yahya <hasna786yahya@gmail.com>
2024-07-01 00:46:54 +0500

    feat: Add lazy loading for images in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Playground } from './components/component/playground';
+import Apology from './components/component/apology';
+
+const isDesktop = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    const isMobile = /mobile|android|iphone|ipad|tablet|touch|samsung|fridge/i.test(userAgent);
+    return !isMobile;
+};
 
 function App() {
-    return (
-        <Playground />
-    )
+    const [isDesktopDevice, setIsDesktopDevice] = React.useState(isDesktop());
+    if (!isDesktopDevice) {
+        return <Apology />;
+    }
+    else {
+        return (
+            <Playground />
+        )
+    }
 }
 
 export default App
